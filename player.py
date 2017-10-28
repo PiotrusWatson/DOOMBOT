b6b5bfe4711c30581b13195aa16f996720177119
PiotrusWatson <piotruswatson@gmail.com>
2017-10-28 07:15:35 +0100

    mild refactor so that people are less confused?

--- player.py
+++ player.py
@@ -8,17 +8,19 @@ class Player:
     def __init__(self, doom_session):
         self.session = doom_session
         self.forwards = True
-        self.turning =  False
-
-
+        self.forwardSpeed = 12
+        self.rotateSpeed = 5 #set too high and we infinite loop
+        self.state = {} #stores EVERYTHING about player (from API)
 
     def turn(self, angle):
         if (angle < 0):
             action_type = "turn-left"
         else:
             action_type = "turn-right"
+            
         self.session.sendAction({"type": action_type, "amount": abs(angle)})
 #todo: make nicer using factories (maybe dicts of dicts?)
+        
     def shoot(self):
         self.session.sendAction({"type": "shoot"})
     
@@ -38,33 +40,39 @@ class Player:
 
         self.session.sendAction({"type": action_type, "amount": abs(amount)})
 
+"""
+turns towards a specific point 
+"""
     def turn_to(self, x_move, y_move):
+        
         x,y,_ = self.state.get("position", {}).values()
 
-        print(f"X: {x_move}, Y: {y_move}\n\n")
         angle = maths.degrees(maths.atan2(y_move-y , x_move-x)) 
         angle = angle if angle >= 0 else angle + 360
 
+        
         player_angle = self.state.get("angle", 0)
         turn_angle = (player_angle-angle)
         
-        if (not self.turning):
-            self.turn(maths.copysign(5, turn_angle))
-            self.turning = True
+        self.turn(maths.copysign(self.rotateSpeed, turn_angle))
 
+"""
+turns to a specific point _ moves TODO RENAME + MAKE PROPER STRAFES
+"""
 
     def move_to(self, x,y):
         self.turn_to(x,y)
         self.forwards = True
 
+"""
+GETS ANOTHER PLAYER FROM ??? + TURNS/MOVES
+"""
     def move_to_player(self, other):
         self.move_to(*list(other.get("position").values())[:2] )
 
     def update(self):
         if (self.forwards):
-            self.move_y(12)
-        if (self.turning):
-            self.turning = False
-
-
-   
+            self.move_y(forwardSpeed)
+    
+    def updateState(self, state):
+        self.state = state
